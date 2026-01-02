23704d6205897a7bb34d957365c5d1da05a91d77
Nikita <mrterran879@gmail.com>
2026-01-02 20:20:36 +0300

    db

--- server/database/database.js
+++ server/database/database.js
@@ -1,20 +1,4 @@
 const path = require("path");
 const { DatabaseSync } = require("node:sqlite");
-const db = new DatabaseSync(path.resolve(__filename,"/data1.db"));
+const db = new DatabaseSync(path.resolve(__dirname,"data.db"));
 
-db.exec(`
-    CREATE TABLE data(
-    key INTEGER PRIMARY KEY,
-    value TEXT
-    ) STRICT    
-`);
-
-const insert = db.prepare(`INSERT INTO data (key,value) VALUES (?,?)`);
-insert.run(1,"TEXT1");
-insert.run(2,"TEXT2");
-insert.run(3,"TEXT3");
-insert.run(4,"TEXT4");
-insert.run(5,"TEXT5");
-
-const query = db.prepare(`SELECT * FROM data ORDER BY key`);
-console.log(query.all());
